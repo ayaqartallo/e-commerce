b29ae6446df3b954867e1b054a79602e8a73a3c1
unknown <aya.q2000@gmail.com>
2023-08-21 11:56:56 +0300

    files project

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/1.jpeg'
+import product2 from './assets/2.jpeg'
+import product3 from './assets/3.jpeg'
+import product4 from './assets/4.jpeg'
+import product5 from './assets/5.jpeg'
+import product6 from './assets/6.jpeg'
+import product7 from './assets/7.jpeg'
+import product8 from './assets/8.jpeg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Laptop",
+        price: 1999.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Camera",
+        price: 699.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Jacket",
+        price: 228.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "LED Light Strips",
+        price: 19.99,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Summer shirt",
+        price: 68.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "T-Shirt",
+        price: 120.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Cap",
+        price: 40.0,
+        productImage: product8,
+    }
+]
